e143d36b53fec906ee03f391fe8f8387972574a5
EduAZ86 <eduardoayaviri@gmail.com>
2024-05-18 01:58:57 -0300

    feature/ cpu.controller

--- src/bench-data/bench-data.service.ts
+++ src/bench-data/bench-data.service.ts
@@ -4,6 +4,7 @@ import { UpdateBenchDataDto } from './dto/update-bench-data.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { DeviceDataDocument } from './schema/bench-data.schema';
+import { DeviceType } from './dto/device-type';
 
 @Injectable()
 export class BenchDataService {
@@ -28,12 +29,12 @@ export class BenchDataService {
     }
 
 
-    async create(deviceType: keyof typeof this.DeviceModels, createBenchDataDto: CreateBenchDataDto) {
+    async create(deviceType: DeviceType, createBenchDataDto: CreateBenchDataDto) {
         const deviceCreated = await this.DeviceModels[deviceType].create(createBenchDataDto)
         return deviceCreated
     }
 
-    async findAll(deviceType: keyof typeof this.DeviceModels, lengthPage: number, offset: number,) {
+    async findAll(deviceType: DeviceType, lengthPage: number, offset: number,) {
         let query = this.DeviceModels[deviceType].find().skip(offset);
         if (lengthPage > 0) {
             query = query.limit(lengthPage);
@@ -42,17 +43,17 @@ export class BenchDataService {
         return foundDevices;
     }
 
-    async findOne(deviceType: keyof typeof this.DeviceModels, id: number) {
+    async findOne(deviceType: DeviceType, id: string) {
         const findDevice = await this.DeviceModels[deviceType].findById(id)
         return findDevice
     }
 
-    async update(deviceType: keyof typeof this.DeviceModels, id: number, updateBenchDataDto: UpdateBenchDataDto) {
+    async update(deviceType: DeviceType, id: string, updateBenchDataDto: UpdateBenchDataDto) {
         const updatedDevice = await this.DeviceModels[deviceType].findByIdAndUpdate(id, updateBenchDataDto, { new: true });
         return updatedDevice
     }
 
-    async remove(deviceType: keyof typeof this.DeviceModels, id: number) {
+    async remove(deviceType: DeviceType, id: string) {
         const deletedDevice = await this.DeviceModels[deviceType].findByIdAndDelete(id);
         return deletedDevice
     }
--- src/bench-data/cpu/cpu.controller.ts
+++ src/bench-data/cpu/cpu.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { BenchDataService } from '../bench-data.service'
 import { CreateBenchDataDto } from '../dto/create-bench-data.dto';
 import { UpdateBenchDataDto } from '../dto/update-bench-data.dto';
@@ -9,26 +9,26 @@ export class CpuController {
 
   @Post()
   create(@Body() createCpuDto: CreateBenchDataDto) {
-    return this.benchDataService.create(createCpuDto);
+    return this.benchDataService.create('cpuData', createCpuDto);
   }
 
   @Get()
-  findAll() {
-    return this.benchDataService.findAll();
+  findAll(@Query('lengthPage') lengthPage: number, @Query('offset') offset: number) {
+    return this.benchDataService.findAll('cpuData', lengthPage, offset);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.benchDataService.findOne(+id);
+    return this.benchDataService.findOne('cpuData', id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCpuDto: UpdateBenchDataDto) {
-    return this.benchDataService.update(+id, updateCpuDto);
+    return this.benchDataService.update('cpuData', id, updateCpuDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.benchDataService.remove(+id);
+    return this.benchDataService.remove('cpuData', id);
   }
 }
